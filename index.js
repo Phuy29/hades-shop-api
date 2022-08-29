f6d0575145786409c8ac205672449b2a619ab2b6
Phuy <phuchuy292003@gmail.com>
2022-08-29 21:03:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -43,27 +43,29 @@ app.get("/collections/:collectionId", async (req, resp) => {
   const allProducts = [];
 
   try {
-    const res = await axios(url);
-    const html = await res.data;
-    const $ = cheerio.load(html);
-    $(".product-block", html).each(function () {
-      const name = $(this).find(".pro-name > a").attr("title");
-      const href = $(this).find(".pro-name > a").attr("href");
-      const price = $(this)
-        .find(".pro-price")
-        .text()
-        .split("\n\t\t\t\t\t\t\n\t\t\t\t\t")[0];
-      const imgUrl = $(this).find(".img-loop").attr("src");
-      const imgHover = $(this).find(".img-hover").attr("src");
-      allProducts.push({
-        name,
-        href,
-        price,
-        imgUrl,
-        imgHoverUrl: imgHover,
+    axios(url).then((res) => {
+      const html = res.data;
+      const $ = cheerio.load(html);
+
+      $(".product-block", html).each(function () {
+        const name = $(this).find(".pro-name > a").attr("title");
+        const href = $(this).find(".pro-name > a").attr("href");
+        const price = $(this)
+          .find(".pro-price")
+          .text()
+          .split("\n\t\t\t\t\t\t\n\t\t\t\t\t")[0];
+        const imgUrl = $(this).find(".img-loop").attr("src");
+        const imgHover = $(this).find(".img-hover").attr("src");
+        allProducts.push({
+          name,
+          href,
+          price,
+          imgUrl,
+          imgHoverUrl: imgHover,
+        });
       });
+      resp.status(200).json(allProducts);
     });
-    await resp.status(200).json(allProducts);
   } catch (error) {
     resp.status(200).json(error);
   }
